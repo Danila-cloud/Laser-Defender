dca0b16176da40eb4c549899018717d68a37cbb4
Danya Provilskyi <65830545+Danila-cloud@users.noreply.github.com>
2022-06-14 20:15:15 +0200

    add coroutine

--- Assets/Scripts/EnemySpawn.cs
+++ Assets/Scripts/EnemySpawn.cs
@@ -1,25 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class EnemySpawn : MonoBehaviour
 {
-    [SerializeField] WaveConfig _waveConfig;
+    [SerializeField] private List<WaveConfig> Waves;
+    [SerializeField] private float TimeBetweenWaves = 1f;
+    private WaveConfig _waveConfig;
     void Start()
     {
-        Spawn();
+        StartCoroutine(SpawnEnemyWaves());
     }
 
     public WaveConfig GetWaveConfig()
     {
         return _waveConfig;
     }
-    void Spawn()
+    IEnumerator SpawnEnemyWaves()
     {
-        for (int i = 0; i < _waveConfig.GetEnemyCount(); i++)
+        While()
         {
-            Instantiate(_waveConfig.GetEnemyPrefab(i), _waveConfig.GetStartingWaypoint().position, Quaternion.identity, transform);
+            foreach (WaveConfig Wave in Waves)
+            {
+                _waveConfig = Wave;
+                for (int i = 0; i < _waveConfig.GetEnemyCount(); i++)
+                {
+                    Instantiate(_waveConfig.GetEnemyPrefab(i), _waveConfig.GetStartingWaypoint().position, Quaternion.identity,
+                        transform);
+                    yield return new WaitForSeconds(_waveConfig.GetRandomSpawnTime());
+                }
 
+                yield return new WaitForSeconds(TimeBetweenWaves);
+            }
         }
+        
     }
 }
--- Assets/Scripts/WaveConfig.cs
+++ Assets/Scripts/WaveConfig.cs
@@ -7,12 +7,22 @@ public class WaveConfig : ScriptableObject
 {
     [SerializeField] List<GameObject> EnemyPrefab;
     [SerializeField] Transform PathPrefab;
+    private float TimeBetweenEnemySpawn = 1f;
+    [SerializeField] float SpawnTimeVariance = 0f;
+    private float MinimumSpawnTime = 0.2f;
 
     [SerializeField] float moveSpeed = 5f;
 
     public Transform GetStartingWaypoint()
     {
         return PathPrefab.GetChild(0);
+    }
+
+    public float GetRandomSpawnTime()
+    {
+        float SpawnTime = Random.Range(TimeBetweenEnemySpawn - SpawnTimeVariance,
+            TimeBetweenEnemySpawn + SpawnTimeVariance);
+        return Mathf.Clamp(SpawnTime, MinimumSpawnTime, float.MaxValue);
     }
 
     public int GetEnemyCount()
